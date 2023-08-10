80143f5b749db1babdd1c7073633397bfb7a8fbd
Lomzem <36811841+Lomzem@users.noreply.github.com>
2023-08-09 18:40:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,27 +7,27 @@ document.getElementById("inputBox").addEventListener("keyup", function(event) {
 function mangaSearch() {
     const inputElement = document.getElementById("inputBox");
     var inputBox = inputElement.value.trim();
-    const aot = "https://mangareader.to/read/attack-on-titan-37/en/chapter-";
-    const tgr = "https://mangareader.to/read/tokyo-ghoulre-362/en/chapter-";
-    const jjk = "https://mangareader.to/read/jujutsu-kaisen-168/en/chapter-";
-    const csm = "https://mangareader.to/read/chainsaw-man-96/en/chapter-";
+    const aot = "https://allmanga.to/read/Cib2WLnvrN46JD9As/shingeki-no-kyojin/chapter-";
+    const tgr = "https://allmanga.to/read/TAehkr5q98ZHZous2/tokyo-ghoul-re/chapter-";
+    const jjk = "https://allmanga.to/read/jMiizAXy49bQ4vpYi/jujutsu-kaisen/chapter-";
+    const csm = "https://allmanga.to/read/boMxbrZ8AB8H7qMvv/chainsaw-man/chapter-";
     
     splitInput = inputBox.split(" ");
 
     if (splitInput[0] == "tgr") {
-        window.location.href = tgr + splitInput[1];
+        window.location.href = tgr + splitInput[1] + "-sub";
     }
 
     else if (splitInput[0] == "jjk") {
-        window.location.href = jjk + splitInput[1];
+        window.location.href = jjk + splitInput[1] + "-sub";
     }
 
     else if (splitInput[0] == "csm") {
-        window.location.href = csm + splitInput[1];
+        window.location.href = csm + splitInput[1] + "-sub";
     }
 
     else if (splitInput[0] == "aot") {
-        window.location.href = aot + splitInput[1];
+        window.location.href = aot + splitInput[1] + "-sub";;
     }
 
     else {
